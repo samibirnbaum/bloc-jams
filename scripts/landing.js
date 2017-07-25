dc472bf3fbf553adb4692e0260847d8a54a58ac5
Sami Birnbaum <sami_birnbaum@btconnect.com>
2017-07-25 15:58:45 +0000

    C9 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
+var points = document.getElementsByClassName("point");
+
 var animatePoints = function (){
               
-    var points=document.getElementsByClassName("point");
-    
     var revealPoint = function() {
         for(var i=0; i<points.length; i++) {
             points[i].style.opacity=1;
@@ -12,3 +12,19 @@ var animatePoints = function (){
     };
     revealPoint();
 };
+
+window.onload = function () {
+    // if window is big enough to display top of selling points container = NO SCROLL EVENT REQUIRED    
+    if (window.innerHeight > 950) {
+        animatePoints();
+     }
+    //  if window doesn't display top of selling points container = YES SCROLL EVENT REQUIRED
+    var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener("scroll",function(event){
+         if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+             animatePoints();
+         }
+    });
+};
+

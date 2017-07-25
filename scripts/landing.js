de46a7ba64a243f3bf9fbb9b19c03f2cdb0ff3c4
Sami Birnbaum <sami_birnbaum@btconnect.com>
2017-07-25 15:58:45 +0000

    C9 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
+var points = document.getElementsByClassName("point");
+
 var animatePoints = function (){
               
-    var points=document.getElementsByClassName("point");
-    
     var revealPoint = function() {
         for(var i=0; i<points.length; i++) {
             points[i].style.opacity=1;
@@ -12,3 +12,20 @@ var animatePoints = function (){
     };
     revealPoint();
 };
+
+window.onload = function () {
+        // if window is big enough to display all content animate immediatley    
+         if (window.innerHeight > 950) {
+         animatePoints();
+     }
+    //  if window doesn't display all content immediatley, wait for my scroll
+    var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    // this sets the scroll distance for the animation to trigger
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener("scroll",function(event){
+         if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+             animatePoints();
+         }
+    });
+};
+

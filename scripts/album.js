82c28d96f9e580488dbc6101e9c14463537ca4ec
Sami Birnbaum <sami_birnbaum@btconnect.com>
2017-08-02 11:50:09 +0000

    A11 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,25 @@ var albumMarconi = {
     ]
 };
 
+var albumGreenDay = {
+    title: 'Dookie',
+    artist: 'Green Day',
+    label: 'MonkeyFuss',
+    year: '1994',
+    albumArtUrl: 'https://upload.wikimedia.org/wikipedia/en/4/4b/Green_Day_-_Dookie_cover.jpg',
+    songs: [
+        { title: 'Burnout', duration: '2:07' },
+        { title: 'Having a Blast', duration: '2:44' },
+        { title: 'Chump', duration: '2:54'},
+        { title: 'Longview', duration: '3:59' },
+        { title: 'Welcome to Paradise', duration: '3:44'},
+        { title: 'Pulling Teeth', duration: '2:31'},
+        { title: 'Basket Case', duration: '3:01'},
+        { title: 'Sassafras Roots', duration: '2:37'},
+        { title: 'When I Come Around', duration: '2:58'}
+    ]
+};
+
 // one purpose function to create the html for the song row and insert its info
 var createSongRow = function (songNumber, songName, songLength) {
     var template = 
@@ -70,3 +89,30 @@ var setCurrentAlbum = function (album) {
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
 };
+
+// album cover when clicked switches between 3 albums
+//My Attempt
+var albumPic = document.getElementsByClassName("album-cover-art")[0]
+albumPic.addEventListener("click", function(event) {
+    if (albumPic.getAttribute("src") == albumGreenDay.albumArtUrl || albumPic.getAttribute("src") == albumPicasso.albumArtUrl ) {
+        setCurrentAlbum(albumMarconi);
+    }
+    else if (albumPic.getAttribute("src") == albumMarconi.albumArtUrl || albumPic.getAttribute("src") == albumGreenDay.albumArtUrl ) {
+        setCurrentAlbum(albumPicasso);
+    }
+    // my code never reaches this statment
+    else if (albumPic.getAttribute("src") == albumMarconi.albumArtUrl || albumPic.getAttribute("src") == albumPicasso.albumArtUrl ) {
+        setCurrentAlbum(albumGreenDay);
+    }
+});
+
+// Blocs Solution
+// var albumsArray = [albumMarconi,albumGreenDay,albumPicasso];
+// var index = 0;
+// document.getElementsByClassName("album-cover-art")[0].addEventListener("click", function(event) {
+//   setCurrentAlbum(albumsArray[index]);
+//   index++; //this changes the var in the global scope where it is retained for the next click
+//   if (index === albumsArray.length){
+//       index=0;//also retained in the global scope for next click
+//   }
+// });
